2fa8b89c9be315dcb097a44a398d59f5753bf6f5
Filipe Mangue <43821186+FilipeMangue@users.noreply.github.com>
2023-08-08 11:55:35 +0200

    Ficheiro de Variaveis
    Colocando ficheiro de variaveis

---
+++ variaveis.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php  
+        /**Criacao de variaveis */
+        $nome;
+        $idade;
+        $nota;
+        $estado;
+
+        /**Atribuicao de valores as variaveis */
+        $nome = "Hello world! <br> Valdimar Fonseca";
+        $idade = 24;
+        $nota = 12.3;
+        $estado = false;
+ 
+        /**Impressao do valor da variavel $nota */
+        echo $nome;
+    ?>
+</body>
+</html>
